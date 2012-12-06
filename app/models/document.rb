41cb2b1e75f26cb678f07d81289348c64d549fa5
hiroyuki inoue <syslink.h.inoue@gmail.com>
2012-12-06 20:17:03 +0900

    filesディレクトリが存在しない場合新規に作成する

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,6 @@
 class Document < ActiveRecord::Base
+  DOCUMENT_DIRECTORY = File.join(Rails.root, "files")
+  
   belongs_to :memo
   
   attr_accessor :upload_data
@@ -7,16 +9,17 @@ class Document < ActiveRecord::Base
   before_save :save_upload_data
   
   def full_path
-    File.join(Rails.root, "files", self.path)
+    File.join(DOCUMENT_DIRECTORY, self.path)
   end
   
   private
     def save_upload_data
+      Dir.mkdir DOCUMENT_DIRECTORY unless Dir.exist? DOCUMENT_DIRECTORY
       File.open(original_filename, "wb"){|f| f.write self.upload_data.read}
       self.path = self.upload_data.original_filename
     end
     
     def original_filename
-      File.join(Rails.root, "files", self.upload_data.original_filename)
+      File.join(DOCUMENT_DIRECTORY, self.upload_data.original_filename)
     end
 end
