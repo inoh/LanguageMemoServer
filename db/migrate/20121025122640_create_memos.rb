e9ac5f18014333507ee4d6dd558a99de0bfa3eda
hiroyuki inoue <syslink.h.inoue@gmail.com>
2012-12-20 19:30:33 +0900

    メモの内容が長いと切れてしまう対応

--- db/migrate/20121025122640_create_memos.rb
+++ db/migrate/20121025122640_create_memos.rb
@@ -2,7 +2,7 @@ class CreateMemos < ActiveRecord::Migration
   def change
     create_table :memos do |t|
       t.string :title
-      t.string :note
+      t.text :note
 
       t.timestamps
     end
