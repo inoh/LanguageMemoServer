0c895c769a4b71bcc4b7cd62ed79f2d0cc34795b
hiroyuki inoue <syslink.h.inoue@gmail.com>
2012-12-22 22:00:34 +0900

    写真アップロード時にカメラ起動対応 クロスドメイン対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  after_filter :response_allow_origin
+
+  private
+    def response_allow_origin
+      response.headers['Access-Control-Allow-Origin'] = "*"
+    end
 end
